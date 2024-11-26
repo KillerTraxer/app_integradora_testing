81055629fb87bd79fedeec5e352c5f30070844f3
KillerTraxer <eduardogf312@gmail.com>
2024-11-26 00:29:47 -0600

    encoded firebase credentials

--- Server/src/firebase.js
+++ Server/src/firebase.js
@@ -1,19 +1,10 @@
 import admin from 'firebase-admin';
+const credential = JSON.parse(
+    Buffer.from(process.env.GOOGLE_SERVICE_KEY, 'base64').toString()
+);
 
 admin.initializeApp({
-    credential: admin.credential.cert({
-        type: process.env.type,
-        project_id: process.env.project_id,
-        private_key_id: process.env.private_key_id,
-        private_key: process.env.private_key,
-        client_email: process.env.client_email,
-        client_id: process.env.client_id,
-        auth_uri: process.env.auth_uri,
-        token_uri: process.env.token_uri,
-        auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-        client_x509_cert_url: process.env.client_x509_cert_url,
-        universe_domain: process.env.universe_domain
-    }),
+    credential: admin.credential.cert(credential),
     databaseURL: "https://cio-dental-default-rtdb.firebaseio.com"
 });
 
