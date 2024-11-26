6273d2a07fe57f17c2b502bedcb74355e431ed0e
KillerTraxer <eduardogf312@gmail.com>
2024-11-25 23:44:49 -0600

    Changed credential of firebase

--- Server/src/firebase.js
+++ Server/src/firebase.js
@@ -1,8 +1,19 @@
 import admin from 'firebase-admin';
-import credenciales from './secret/cio-dental-firebase-adminsdk-umsiy-1788b0fbab.json' assert { type: 'json' };
 
 admin.initializeApp({
-    credential: admin.credential.cert(credenciales),
+    credential: admin.credential.cert({
+        type: process.env.type,
+        project_id: process.env.project_id,
+        private_key_id: process.env.private_key_id,
+        private_key: process.env.private_key,
+        client_email: process.env.client_email,
+        client_id: process.env.client_id,
+        auth_uri: process.env.auth_uri,
+        token_uri: process.env.token_uri,
+        auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+        client_x509_cert_url: process.env.client_x509_cert_url,
+        universe_domain: process.env.universe_domain
+    }),
     databaseURL: "https://cio-dental-default-rtdb.firebaseio.com"
 });
 
